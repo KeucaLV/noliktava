7c321d7b01375fbabe5349cad2c39fb4171d4e2e
Diablo785 <96918963+Diablo785@users.noreply.github.com>
2023-12-12 15:20:28 +0200

    Add files via upload

--- src/components/PlauktuKartotajsPreces.jsx
+++ src/components/PlauktuKartotajsPreces.jsx
@@ -6,7 +6,7 @@ function PlauktuKartotajsPreces() {
     <div className="nez">
         <div className="PlauktuKartotajsPreces">
             <div className="SearchContainer">
-                <input type="text" placeholder="Search..." className="SearchInput" />
+                <input type="text" placeholder="Meklēt..." className="SearchInput" />
 
                 <select className="Dropdown">
                 <option value="plaukts">Plaukts</option>
@@ -21,7 +21,15 @@ function PlauktuKartotajsPreces() {
                 </select>
             </div>
             <div className="Preces">
-
+              <div className="item">1</div>
+              <div className="item">2</div>
+              <div className="item">3</div>
+              <div className="item">4</div>
+              <div className="item">5</div>
+              <div className="item">6</div>
+              <div className="item">7</div>
+              <div className="item">8</div>
+              <div className="item">9</div>
             </div>
         </div>
     </div>
